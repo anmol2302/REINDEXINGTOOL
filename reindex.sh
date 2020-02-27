e412cd828e8a3059f2abce7057db532ee861c3ad
anmol2302 <anmolgupta23021997@gmail.com>
2020-02-27 13:29:58 +0530

    backup file generated basis of timestamp

--- reindex.sh
+++ reindex.sh
@@ -337,8 +337,9 @@ else
   exit 1
 fi
 
-
-echo "PERFORMING '$old_index' BACKUP, can be found in file certregBackup.txt"
+DATE=`date "+%Y%m%d-%H%M%S"`
+backup_file_name=certregBackup$DATE.txt
+echo "PERFORMING '$old_index' BACKUP, can be found in file $backup_file_name"
 
 curl --location --request GET 'http://'$es_ip':9200/'$old_index'/_search' --header 'Content-Type: application/json' --data-raw '{
    "query":{
@@ -348,7 +349,7 @@ curl --location --request GET 'http://'$es_ip':9200/'$old_index'/_search' --head
    }
 }
 
-' | jq > certregBackup.txt
+' | jq > $backup_file_name
 
 
 data_count=$( curl --location --request GET 'http://'$es_ip':9200/'$old_index'/_count' --header 'Content-Type: application/json' --header 'Accept: text' | jq ."count" )
